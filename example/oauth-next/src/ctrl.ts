e87690283830205eb93fae22c070ae41ffb3b737
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-06-14 18:19:26 +0200

    handle errors

--- example/oauth-next/src/ctrl.ts
+++ example/oauth-next/src/ctrl.ts
@@ -1,11 +1,18 @@
 export type State = 'unauthorized' | 'preparing' | 'error' | 'verifying' | 'authorized';
 
-const authorizeEndpoint = 'http://localhost:9663/oauth';
+const authorizeEndpoint = 'http://localhost:9663/oauth/authorize';
 
 export class Ctrl {
   state: State = 'unauthorized';
 
+  // https://www.oauth.com/oauth2-servers/server-side-apps/possible-errors/
+  error: string | null;
+  errorDescription: string | null;
+
   constructor(private redraw: () => void) {
+    const params = new URL(location.href).searchParams;
+    this.error = params.get('error');
+    this.errorDescription = params.get('error_description');
   }
 
   async login() {
@@ -14,13 +21,15 @@ export class Ctrl {
 
     // 1. Generate cryptographically random code verifier.
     const codeVerifier = secureRandom(64);
+    sessionStorage.setItem('code_verifier', encodeBase64Url(codeVerifier));
 
     // 2. Derive code challenge from code verifier.
     const digest = new Uint8Array(await crypto.subtle.digest('SHA-256', codeVerifier));
-    const codeChallenge = base64Url(digest);
+    const codeChallenge = encodeBase64Url(digest);
 
     // 3. Generate random state (arbitrarily).
-    const state = base64Url(secureRandom(64));
+    const state = encodeBase64Url(secureRandom(64));
+    sessionStorage.setItem('state', state);
 
     // 4. Redirect to authorization endpoint.
     const redirectUri = new URL(location.href);
@@ -32,6 +41,7 @@ export class Ctrl {
     url.searchParams.set('state', state);
     url.searchParams.set('code_challenge', codeChallenge);
     url.searchParams.set('code_challenge_method', 'S256');
+    url.searchParams.set('scope', ['email:read'].join(' '));
     location.href = url.href;
   }
 }
@@ -42,6 +52,6 @@ function secureRandom(bytes: number): Uint8Array {
   return buffer;
 }
 
-function base64Url(bytes: Uint8Array): string {
+function encodeBase64Url(bytes: Uint8Array): string {
   return btoa(String.fromCharCode(...bytes)).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
 }
--- example/oauth-next/src/view.ts
+++ example/oauth-next/src/view.ts
@@ -4,17 +4,28 @@ import { Ctrl } from './ctrl';
 export function view(ctrl: Ctrl): VNode {
   return h('table', [
     h('tr', [
-      h('td', 'State'),
+      h('td', 'Application state'),
       h('td', ctrl.state),
     ]),
     h('tr', [
-      h('td', 'Action'),
+      h('td', 'Error'),
+      h('td', ctrl.error),
+    ]),
+    h('tr', [
+      h('td', 'Error description'),
+      h('td', ctrl.errorDescription),
+    ]),
+    h('tr', [
+      h('td', ''),
       h('td', [
-        ctrl.state == 'unauthorized' ? h('button', {
+        h('button', {
+          attrs: {
+            disabled: ctrl.state != 'unauthorized',
+          },
           on: {
-            click: () => ctrl.login(),
+           click: () => ctrl.login(),
           }
-        }, 'Login') : undefined,
+        }, 'Login'),
       ]),
     ]),
   ]);
