fb073fa0f257c6bc80ac14409b3258ce825b1fe4
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-06-18 12:20:33 +0200

    add some more comments for pkce example

--- example/oauth-app/src/ctrl.ts
+++ example/oauth-app/src/ctrl.ts
@@ -1,6 +1,6 @@
 import { AccessContext, HttpClient, OAuth2AuthCodePKCE } from '@bity/oauth2-auth-code-pkce';
 
-export const lichessHost = 'http://localhost:9663';
+export const lichessHost = 'https://lichess.org';
 export const clientId = 'example.com';
 export const clientUrl = (() => {
   const url = new URL(location.href);
@@ -9,7 +9,7 @@ export const clientUrl = (() => {
 })();
 
 export class Ctrl {
-  oauthClient: OAuth2AuthCodePKCE = new OAuth2AuthCodePKCE({
+  oauth = new OAuth2AuthCodePKCE({
     authorizationUrl: `${lichessHost}/oauth`,
     tokenUrl: `${lichessHost}/api/token`,
     clientId,
@@ -20,7 +20,7 @@ export class Ctrl {
   });
 
   error?: any;
-  accessToken?: AccessContext;
+  accessContext?: AccessContext;
 
   email?: string;
 
@@ -28,15 +28,16 @@ export class Ctrl {
 
   async init() {
     try {
-      const hasAuthCode = await this.oauthClient.isReturningFromAuthServer();
+      const hasAuthCode = await this.oauth.isReturningFromAuthServer();
       if (hasAuthCode) {
-        // Might want to persist the token in session storage.
-        this.accessToken = await this.oauthClient.getAccessToken();
+        // Might want to persist accessContext.token until the user logs out.
+        this.accessContext = await this.oauth.getAccessToken();
         this.redraw();
 
         // Can also use this convenience wrapper for fetch() instead of
-        // using the raw token with getAccessToken().
-        const fetch = this.oauthClient.decorateFetchHTTPClient(window.fetch);
+        // using manually using getAccessToken() and setting the
+        // "Authorization: Bearer ..." header.
+        const fetch = this.oauth.decorateFetchHTTPClient(window.fetch);
         await this.useApi(fetch);
       }
     } catch (err) {
@@ -46,10 +47,12 @@ export class Ctrl {
   }
 
   async login() {
-    await this.oauthClient.fetchAuthorizationCode();
+    await this.oauth.fetchAuthorizationCode();
   }
 
   async useApi(fetch: HttpClient) {
+    // Requests will fail with 401 Unauthorized if the access token expired
+    // or was revoked. Make sure to offer a chance to reauthenticate.
     const res = await fetch(`${lichessHost}/api/account/email`);
     this.email = (await res.json()).email;
     this.redraw();
@@ -57,7 +60,7 @@ export class Ctrl {
 
   logout() {
     this.error = undefined;
-    this.accessToken = undefined;
+    this.accessContext = undefined;
     this.redraw();
   }
 }
--- example/oauth-app/src/view.ts
+++ example/oauth-app/src/view.ts
@@ -13,7 +13,7 @@ export function view(ctrl: Ctrl): VNode {
     h('tr', [h('td', 'Lichess Host'), h('td', lichessHost)]),
     h('tr', [h('td', 'Client URL'), h('td', clientUrl)]),
     h('tr', [h('td', 'Error'), h('td', ctrl.error?.toString())]),
-    h('tr', [h('td', 'Access token (secret)'), h('td', ctrl.accessToken?.token?.value)]),
+    h('tr', [h('td', 'Access token (secret)'), h('td', ctrl.accessContext?.token?.value)]),
     h('tr', [h('td', 'Lichess account email'), h('td', ctrl.email)]),
     h('tr', [
       h('td', ''),
@@ -21,7 +21,7 @@ export function view(ctrl: Ctrl): VNode {
         h(
           'button',
           {
-            attrs: { disabled: !!ctrl.accessToken?.token },
+            attrs: { disabled: !!ctrl.accessContext?.token },
             on: { click: () => ctrl.login() },
           },
           'Login'
@@ -30,10 +30,10 @@ export function view(ctrl: Ctrl): VNode {
         h(
           'button',
           {
-            attrs: { disabled: !ctrl.error && !ctrl.accessToken?.token },
+            attrs: { disabled: !ctrl.error && !ctrl.accessContext?.token },
             on: { click: () => ctrl.logout() },
           },
-          ctrl.accessToken ? 'Logout' : 'Reset'
+          ctrl.accessContext ? 'Logout' : 'Reset'
         ),
       ]),
     ]),
