75e109dd57941b5065634b21f05c4e5ce9ddd59f
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2025-04-26 09:50:47 -0400

    Update broadcast example responses with scripted calls

--- scripts/update-examples/broadcasts.ts
+++ scripts/update-examples/broadcasts.ts
@@ -1,4 +1,4 @@
-import { example, localClient } from "./config";
+import { example, localClient, readTextStream } from "./config";
 
 const newOfficialTournament = await localClient("admin").POST(
   "/broadcast/new",
@@ -30,6 +30,51 @@ for (let i = 1; i <= 3; i++) {
   );
   console.log(`Created round ${round.data!.round.url}`);
 }
+
+const finalRound = await localClient("admin").POST(
+  "/broadcast/{broadcastTournamentId}/new",
+  {
+    params: {
+      path: {
+        broadcastTournamentId: newOfficialTournament.data!.tour.id,
+      },
+    },
+    body: {
+      name: "Final Round",
+    },
+  },
+);
+
+const pushPgn = await localClient("admin").POST(
+  "/api/broadcast/round/{broadcastRoundId}/push",
+  {
+    headers: {
+      "Content-Type": "text/plain",
+    },
+    params: {
+      path: {
+        broadcastRoundId: finalRound.data!.round.id,
+      },
+    },
+    body: `
+[Event "Knight Invitational"]
+[White "Player 1"]
+[Black "Player 2"]
+[Result "*"]
+
+1. e4 e5
+
+
+[Event "Knight Invitational"]
+[White "Player 3"]
+[Black "Player 4"]
+[Result "*"]
+
+1. d4 d5
+`,
+    bodySerializer: (body) => body,
+  },
+);
 
 example(
   "broadcasts",
@@ -78,6 +123,148 @@ example(
     params: {
       path: {
         broadcastTournamentId: newOfficialTournament.data!.tour.id,
+      },
+    },
+  }),
+);
+
+example(
+  "broadcasts",
+  "getPlayersOfBroadcastTournament",
+  await localClient().GET("/broadcast/{broadcastTournamentId}/players", {
+    params: {
+      path: {
+        broadcastTournamentId: newOfficialTournament.data!.tour.id,
+      },
+    },
+  }),
+);
+
+example(
+  "broadcasts",
+  "updateBroadcastTournament",
+  await localClient("admin").POST("/broadcast/{broadcastTournamentId}/edit", {
+    params: {
+      path: {
+        broadcastTournamentId: newOfficialTournament.data!.tour.id,
+      },
+    },
+    body: {
+      name: "Knight Invitational 2",
+    },
+  }),
+);
+
+example("broadcasts", "createBroadcastRound", finalRound);
+
+example(
+  "broadcasts",
+  "getBroadcastRound",
+  await localClient().GET(
+    "/api/broadcast/{broadcastTournamentSlug}/{broadcastRoundSlug}/{broadcastRoundId}",
+    {
+      params: {
+        path: {
+          broadcastTournamentSlug: "-",
+          broadcastRoundSlug: "-",
+          broadcastRoundId: finalRound.data!.round.id,
+        },
+      },
+    },
+  ),
+);
+
+example(
+  "broadcasts",
+  "updateBroadcastRound",
+  await localClient("admin").POST("/broadcast/round/{broadcastRoundId}/edit", {
+    params: {
+      path: {
+        broadcastRoundId: finalRound.data!.round.id,
+      },
+    },
+    body: {
+      name: "Final Round 2",
+    },
+  }),
+);
+
+example("broadcasts", "pushPgnToBroadcastRound", pushPgn);
+
+const abortController = new AbortController();
+const signal = abortController.signal;
+let pgnLines: string[] = [];
+setTimeout(() => abortController.abort(), 1000 * 2);
+await localClient()
+  .GET("/api/stream/broadcast/round/{broadcastRoundId}.pgn", {
+    params: {
+      path: {
+        broadcastRoundId: finalRound.data!.round.id,
+      },
+    },
+    signal,
+    parseAs: "stream",
+  })
+  .then((response) => {
+    readTextStream(response.response, (text: string) => {
+      pgnLines.push(text);
+      if (pgnLines.filter((line) => line === "").length === 6) {
+        const pgn = pgnLines.join("\n");
+        example("broadcasts", "streamOngoingBroadcastRoundAsPgn", pgn, "pgn");
+      }
+    });
+  });
+
+example(
+  "broadcasts",
+  "exportOneRoundAsPgn",
+  await localClient().GET("/api/broadcast/round/{broadcastRoundId}.pgn", {
+    params: {
+      path: {
+        broadcastRoundId: finalRound.data!.round.id,
+      },
+    },
+    parseAs: "text",
+  }),
+  "pgn",
+);
+
+example(
+  "broadcasts",
+  "exportAllRoundsAsPgn",
+  await localClient().GET("/api/broadcast/{broadcastTournamentId}.pgn", {
+    params: {
+      path: {
+        broadcastTournamentId: newOfficialTournament.data!.tour.id,
+      },
+    },
+    parseAs: "text",
+  }),
+  "pgn",
+);
+
+example(
+  "broadcasts",
+  "resetBroadcastRound",
+  await localClient("admin").POST(
+    "/api/broadcast/round/{broadcastRoundId}/reset",
+    {
+      params: {
+        path: {
+          broadcastRoundId: finalRound.data!.round.id,
+        },
+      },
+    },
+  ),
+);
+
+example(
+  "broadcasts",
+  "getYourBroadcastRounds",
+  await localClient("admin").GET("/api/broadcast/my-rounds", {
+    params: {
+      query: {
+        nb: 1,
       },
     },
   }),
--- scripts/update-examples/config.ts
+++ scripts/update-examples/config.ts
@@ -17,7 +17,13 @@ export const prodClient = () =>
     baseUrl: "https://lichess.org",
   });
 
-export function example(category: string, name: string, response: any) {
+export function example(
+  category: string,
+  name: string,
+  response: any,
+  filetype: "json" | "pgn" = "json",
+) {
+  const saveAsYaml = filetype !== "json";
   const filename = join(
     resolve(
       dirname(fileURLToPath(import.meta.url)),
@@ -27,32 +33,81 @@ export function example(category: string, name: string, response: any) {
       "specs",
       "examples",
     ),
-    `${category}-${name}.json`,
+    `${category}-${name}.${filetype}${saveAsYaml ? ".yaml" : ""}`,
   );
   console.log(`Writing ${filename}`);
 
-  let contents = JSON.stringify(response.data ?? response, null, 2);
-  contents = contents.replace("http://localhost:8080", "https://lichess.org");
+  const data: string = response.data ?? response;
+  let contents = saveAsYaml
+    ? convertStringToYaml(data)
+    : JSON.stringify(data, null, 2);
+  contents = contents.replace(
+    /http:\/\/localhost:8080/g,
+    "https://lichess.org",
+  );
 
   writeFileSync(filename, contents);
 }
+
+const convertStringToYaml = (str: string) => {
+  let lines: string[] = ["value: |"];
+  const indent = " ".repeat(2);
+  const strLines = str.split("\n");
+  for (const line of strLines) {
+    lines.push(`${indent}${line}`);
+  }
+  return lines.join("\n");
+};
 
 type ProcessLine<T> = (line: T) => void;
 export const readNdJson = async <T>(
   response: Response,
   processLine: ProcessLine<T>,
 ): Promise<void> => {
-  if (!response.ok) throw new Error(`Status ${response.status}`);
-  const stream = response.body!.getReader();
-  const matcher = /\r?\n/;
-  const decoder = new TextDecoder();
-  let buf = "";
-  let done, value;
-  do {
-    ({ done, value } = await stream.read());
-    buf += decoder.decode(value || new Uint8Array(), { stream: !done });
-    const parts = buf.split(matcher);
-    if (!done) buf = parts.pop()!;
-    for (const part of parts) if (part) processLine(JSON.parse(part));
-  } while (!done);
+  try {
+    if (!response.ok) throw new Error(`Status ${response.status}`);
+    const stream = response.body!.getReader();
+    const matcher = /\r?\n/;
+    const decoder = new TextDecoder();
+    let buf = "";
+    let done, value;
+    do {
+      ({ done, value } = await stream.read());
+      buf += decoder.decode(value || new Uint8Array(), { stream: !done });
+      const parts = buf.split(matcher);
+      if (!done) buf = parts.pop()!;
+      for (const part of parts) if (part) processLine(JSON.parse(part));
+    } while (!done);
+  } catch (error) {
+    if (error instanceof DOMException && error.name === "AbortError") {
+      return;
+    }
+    throw error;
+  }
+};
+
+export const readTextStream = async (
+  response: Response,
+  processLine: (line: string) => void,
+): Promise<void> => {
+  try {
+    if (!response.ok) throw new Error(`Status ${response.status}`);
+    const stream = response.body!.getReader();
+    const matcher = /\r?\n/;
+    const decoder = new TextDecoder();
+    let buf = "";
+    let done, value;
+    do {
+      ({ done, value } = await stream.read());
+      buf += decoder.decode(value || new Uint8Array(), { stream: !done });
+      const parts = buf.split(matcher);
+      if (!done) buf = parts.pop()!;
+      for (const part of parts) processLine(part);
+    } while (!done);
+  } catch (error) {
+    if (error instanceof DOMException && error.name === "AbortError") {
+      return;
+    }
+    throw error;
+  }
 };
