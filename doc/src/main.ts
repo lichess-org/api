25fcc9b02952bbe42d0f50054754be6b5fec761b
Trevor Fitzgerald <fitztrev@gmail.com>
2025-11-17 21:47:56 -0500

    scalar docs

---
+++ doc/src/main.ts
@@ -0,0 +1,8 @@
+import { createApiReference } from '@scalar/api-reference'
+import '@scalar/api-reference/style.css'
+
+createApiReference('#app', {
+  url: '../public/openapi.yaml',
+  withDefaultFonts: false,
+  hideModels: true,
+})
